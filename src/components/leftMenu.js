f983509bcf9192bfb89bc40822ce0aa6ecb8d5ab
newbrine <35578361+newbrine@users.noreply.github.com>
2020-06-08 13:51:24 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/leftMenu.js 	new file:   src/theme.js 	deleted:    yarn.lock

--- src/components/leftMenu.js
+++ src/components/leftMenu.js
@@ -1,48 +1,13 @@
 import React from 'react'
-import { Image, Heading, BlockLink, BackgroundImage, Box, Text, Flex } from 'pcln-design-system'
-import '../leftMenu.css'
+import{ ThemeProvider, Button } from '@chakra-ui/core'
+import styled from '@emotion/styled'
 
 export default function LeftMenu({ task: { id, title, state }, onArchiveTask, onPinTask }){
     return(
-        <div className='left-menu'>
-            <div className='header'>
-                <Image src={'./images/NewsMuzzlerAppIcon.png'} />
-                <Heading m={2}>
-                    NewsMuzzler
-                </Heading>
+        <ThemeProvider>
+            <div>
+                <Button variantColor="green">Button</Button>
             </div>
-            <BlockLink href='https://www.priceline.com' target='_blank'>
-                <Box className='item' p={4}>
-                    <Image src={'./images/ChannelsIcon.png'} />
-                    <Heading m={3}>
-                        Channels
-                    </Heading>
-                </Box>
-            </BlockLink>
-            <BlockLink href='https://www.google.com' target='_blank'>
-                <Box className='item' p={4}>
-                    <Image src={'./images/ContentIcon.png'} />
-                    <Heading m={3}>
-                        Content
-                    </Heading>
-                </Box>
-            </BlockLink>
-            <BlockLink href='https://www.amazon.com' target='_blank'>
-                <Box className='item' p={4}>
-                    <Image src={'./images/SubscriptionIcon.png'} />
-                    <Heading m={3}>
-                        Subscriptions
-                    </Heading>
-                </Box>
-            </BlockLink>
-            <BlockLink href='https://www.twitter.com' target='_blank'>
-                <Box className='item' p={4}>
-                    <Image src={'./images/StatisticsIcon.png'} />
-                    <Heading m={3}>
-                        Statistics
-                </Heading>
-                </Box>
-            </BlockLink>
-        </div>
+        </ThemeProvider>
     )
 }
