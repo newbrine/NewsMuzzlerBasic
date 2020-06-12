759ec779746eecc977991aaf9b7bf30901620577
newbrine <35578361+newbrine@users.noreply.github.com>
2020-06-12 11:35:17 -0500

    new file:   .storybook/manager.js 	modified:   .storybook/preview.js 	new file:   .storybook/storybookTheme.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/subscriptionCard.js 	modified:   src/components/subscriptionCard.stories.js 	deleted:    src/leftMenu.css 	modified:   src/theme.js

--- src/components/subscriptionCard.stories.js
+++ src/components/subscriptionCard.stories.js
@@ -7,4 +7,12 @@ export default{
     title: 'Subscription Card'
 };
 
-export const Default = () => <SubscriptionCard likedNumber="4" savedNumber='6'/>;
+export const Default = () => <SubscriptionCard 
+                                newsletterName='Ask Leo' 
+                                likedNumber="4" 
+                                savedNumber='6'
+                                elements={3}
+                                titles={['Title 1', 'Title 2', 'Title 3']}
+                                dates={['June 9th, 2020 10:37AM', 'June 8th, 2020 10:37AM', 'June 7th, 2020 10:37AM']}
+                                activity={['Sent by source', 'Read by me', 'Sent by source']}
+                             />;
